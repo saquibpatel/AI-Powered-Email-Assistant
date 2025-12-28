73ff47acce7b04e49c6a2f76d0a996fb90ad7fff
i.saquib <saquib.next@gmail.com>
2025-12-28 10:55:18 +0530

    initial setup..

---
+++ src/test/java/com/email/writer/EmailWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
